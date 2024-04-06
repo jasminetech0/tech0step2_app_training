6e811a7b2e70d67fc5b1d947c0ef7e8940041834
jasminetech0 <jiebing.chen.2020@globis.ac.jp>
2024-04-06 14:54:19 +0900

    first commit

---
+++ my_app/pages/main0331.py
@@ -0,0 +1,39 @@
+# streamlitのインポート
+import streamlit as st
+
+# テキストの表示
+st.write("demoです")
+
+# ボタンの表示
+button = st.button("クリック")
+if button:
+    st.write("ボタンがクリックされました")
+
+# 選択ボックス
+options = st.multiselect(
+    'どの寿司が好きですか',
+    ['いか', 'えび', 'まぐろ', 'サーモン'],
+    ["いか"])
+st.write(",".join(options))
+
+
+"""
+git init
+git remote add origin https://github.com/jasminetech0/streamlit_training1.git
+git add .
+git commit -m'1st commit'
+git push origin main
+
+
+streamlit run main.py
+
+"""
+
+# git init
+# git remote add origin https://github.com/jasminetech0/streamlit_training1.git
+# git add .
+# git commit -m'1st commit'
+# git push origin main
+
+
+# streamlit run main.py
